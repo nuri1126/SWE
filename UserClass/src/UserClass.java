6c1c4c50e54aa35f2703993463b3755055ce79be
Jeong <Jeong@HJLee>
2016-12-03 19:22:43 +0900

    memo기능 구현 완료(시작할 때 파일을 읽어와 벡터로 작업 후 다시 파일로 저장)

--- UserClass/src/UserClass.java
+++ UserClass/src/UserClass.java
@@ -1,16 +1,24 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Vector;
 
 public class UserClass {
 	String userId="1311713";
 	String userPassword="a1311713";
-	public UserClass() throws IOException{
+	public UserClass() throws IOException {
 		login();
 	}
-	public void login() throws IOException{
+	public void login() throws IOException {
 		System.out.print("회원님의 아이디를 입력해주세요. \n>>>");
 		InputStreamReader inputStreamReader = new InputStreamReader(System.in);
 		BufferedReader inputId = new BufferedReader(inputStreamReader);
@@ -23,7 +31,7 @@ public class UserClass {
 			}else{System.out.print("틀렸습니다.");login();}
 		}else{System.out.print("틀렸습니다.");login();}
 	}
-	public void userMain() throws IOException{
+	public void userMain() throws IOException {
 		InputStreamReader inputStreamReader = new InputStreamReader(System.in);
 		BufferedReader inputNumber = new BufferedReader(inputStreamReader);
 		while(true){
@@ -55,22 +63,29 @@ public class UserClass {
 		UserClass user = new UserClass();
 	}
 }
-class Memo {
+class Memo implements Serializable {
 	private String memoContent;
 	private int memoNumber;
 	private String memoDate;
-	
 	public Memo(String memoContent, int memoNumber, String memoDate) {
 		this.memoContent = memoContent;
 		this.memoNumber = memoNumber;
 		this.memoDate = memoDate;
 	}
+	public int getMemoNumber() {
+		return memoNumber;
+	}
 	public void setMemoNumber(int memoNumber) {
 		this.memoNumber = memoNumber;
+	}
+	@Override
+	public String toString() {
+		return "["+memoNumber+"]"+"Content : " + memoContent + ", Date : " + memoDate;
 	}
 }
 class MemoClass {
 	InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+	Vector<Memo> memoVector = new Vector<Memo>();
 	final int initialMemoNumber = -1;
 	public MemoClass() throws IOException {
 		memoMain();
@@ -78,7 +93,16 @@ class MemoClass {
 	void memoMain() throws IOException {
 		BufferedReader inputNumber = new BufferedReader(inputStreamReader);
 		boolean managingMemo = true;
+		File file = new File("memo.txt");
+		if(file.exists()){
+			try {
+				fileOpen();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
 		while(managingMemo){
+			System.out.println("----------MEMO----------");
 			System.out.println("원하는 작업을 입력하세요.");
 			System.out.println("1.Add Memo");
 			System.out.println("2.View All Memo");
@@ -86,31 +110,42 @@ class MemoClass {
 			System.out.println("0.Quit");
 			System.out.print(">>>");
 			switch(inputNumber.readLine()){
-			case "1":
-				add();
-				break;
-			case "2":
-				view();
-				break;
-			case "3":
-				BufferedReader memoNumber = new BufferedReader(inputStreamReader);
-				System.out.println("지울 메모 번호를 입력하세요. \n>>>");
-				int deleteNumber = Integer.parseInt(memoNumber.readLine());
-				delete(deleteNumber);
-				break;
-			case "0":
-				managingMemo = false;
-				break;
+				case "1":
+					add();
+					break;
+				case "2":
+					view();
+					break;
+				case "3":
+					if(memoVector.isEmpty()){
+						System.out.println("메모가 없습니다.");
+						break;
+					}
+					BufferedReader tempNumber = new BufferedReader(inputStreamReader);
+					System.out.println("지울 메모 번호를 입력하세요.");
+					System.out.print(">>>");
+					int memoNumber = Integer.parseInt(tempNumber.readLine());
+					delete(memoNumber);
+					break;
+				case "0":
+					System.out.println("메모 작업에서 나갑니다.");
+					fileSave(memoVector);
+					managingMemo = false;
+					break;
+				default :
+					System.out.println("0~3 사이의 숫자를 입력하세요!");
+					break;
 			}
 		}
 	}
 	void add() throws IOException {
 		BufferedReader tempContent = new BufferedReader(inputStreamReader);
-		System.out.println("메모 내용을 입력하세요. \n>>>");
+		System.out.println("메모 내용을 입력하세요.");
+		System.out.print(">>>");
 		String inputContent = tempContent.readLine();
-		boolean validAnswer = true;
-		while(validAnswer){
-			System.out.println("메모를 추가하시겠습니까?(Y/N) \n>>>");
+		while(true){
+			System.out.println("메모를 추가하시겠습니까?(Y/N)");
+			System.out.print(">>>");
 			BufferedReader tempAnswer = new BufferedReader(inputStreamReader);
 			String answer = tempAnswer.readLine();
 			if(answer.equals("Y")) {
@@ -118,31 +153,49 @@ class MemoClass {
 				SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 				String today = transFormat.format(tempToday);
 				Memo memo = new Memo(inputContent, initialMemoNumber, today);
-				fileSave(memo);
-			}
-			else if(answer.equals("N")){
+				memoVector.add(memo);
+				System.out.println("메모를 저장하였습니다!");
+				updateNumber();
+				break;
+			} else if(answer.equals("N")){
 				System.out.println("메모 작성을 취소합니다.");
 				break;
-			}
-			else{
-				System.out.println("Y 또는 N를 입력해주세요.");
-				validAnswer = false;
-			}
+			} else
+				System.out.println("Y 또는 N를 입력해주세요!");
 		}
 	}
 	void view() {
-		
+		if(memoVector.isEmpty()){
+			System.out.println("메모가 없습니다.");
+			return;
+		}
+		for(int i = 0 ; i < memoVector.size() ; i++){
+			System.out.println(memoVector.get(i).toString());
+		}
 	}
 	void delete(int memoNumber) {
-		
+		for(int i = 0 ; i < memoVector.size() ; i++){
+			Memo tempMemo = memoVector.get(i);
+			if(tempMemo.getMemoNumber()==memoNumber){
+				memoVector.remove(i);
+				updateNumber();
+				System.out.println(memoNumber+"번 메모를 삭제했습니다!");
+				return;
+			}
+		}
+		System.out.println(memoNumber+"번에 해당하는 메모가 없습니다. view메뉴로 메모를 확인하세요.");
 	}
-	void fileSave(Memo memo) {
-		
+	void fileSave(Object memo) throws IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("memo.txt"));
+		oos.writeObject(memo);
+		oos.close();
 	}
-	void fileOpen(Memo memo) {
-		
+	void fileOpen() throws FileNotFoundException, IOException, ClassNotFoundException	{
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("memo.txt"));
+		memoVector = (Vector<Memo>) ois.readObject();
 	}
 	void updateNumber() {
-		
+		for(int i=0 ; i < memoVector.size() ; i++)
+			memoVector.get(i).setMemoNumber(i+1);
 	}
 }
