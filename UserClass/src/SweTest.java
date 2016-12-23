b7f0d9c1f58b59f5acaaee8b69128ba5713aab3e
nuri1126 <nury7230@naver.com>
2016-12-23 23:26:19 +0900

    schedule testcase push

--- UserClass/src/SweTest.java
+++ UserClass/src/SweTest.java
@@ -71,4 +71,13 @@ public class SweTest {
 		assertTrue(memo.delete(size) == true);
 		assertTrue(memo.delete(size/2) == true);
 	}
+	@Test
+	public void testScheduleDataExist() throws IOException {
+		ScheduleClass schedule = new ScheduleClass();
+		assertTrue(schedule.view(0)==true); 
+		assertTrue(schedule.view(-1)==true);
+		assertTrue(schedule.view(10)==true);
+		assertTrue(schedule.view(2000)==true);
+	}
+	
 }
